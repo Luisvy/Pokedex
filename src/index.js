ee935517ae2de049fa674bf3b3cd63f81553e2a0
Luis Avilés <34698896+Luisvy@users.noreply.github.com>
2025-05-23 12:12:20 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-// Creamos la ruta principal con el modulo createRoot proveniente de la clase del index
-const container = document.getElementById("root");
-const root = createRoot(container);
-// Renderizamos
-root.render(<App />);
